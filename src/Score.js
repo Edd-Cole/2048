cdf343db342c9bcd4c91ab40feb65b998d56a44c
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-10-13 16:25:03 +0100

    Changing score counting for hosted version

--- src/Score.js
+++ src/Score.js
@@ -2,7 +2,7 @@ import { createTime } from "./utils";
 const Score = ({moves, time}) => {
     return (
         <section>
-            <h3>Moves: {moves}</h3>
+            <h3>Moves: {moves * 2}</h3>
             <h3>Time: {createTime(time)}</h3>
         </section>
     )
